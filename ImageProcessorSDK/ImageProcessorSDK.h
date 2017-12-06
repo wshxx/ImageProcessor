5e02069215d43dae0882b173bd9f85359f3b68f3
王赛华 <wangsaihua@saihuawangdeMacBook-Air.local>
2017-12-06 17:19:23 +0800

    第一次上传

---
+++ ImageProcessorSDK/ImageProcessorSDK.h
@@ -0,0 +1,19 @@
+//
+//  ImageProcessorSDK.h
+//  ImageProcessorSDK
+//
+//  Created by 王赛华 on 2017/12/6.
+//  Copyright © 2017年 王赛华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageProcessorSDK.
+FOUNDATION_EXPORT double ImageProcessorSDKVersionNumber;
+
+//! Project version string for ImageProcessorSDK.
+FOUNDATION_EXPORT const unsigned char ImageProcessorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageProcessorSDK/PublicHeader.h>
+
+
